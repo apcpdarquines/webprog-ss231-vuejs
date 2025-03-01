9f0cfd8a94881f852f36da9567daec265232bc4b
apcpdarquines <pdarquines@student.apc.edu.ph>
2025-03-01 08:56:48 +0000

    Fooditems

--- vue-project/src/components/Fooditem2.vue
+++ vue-project/src/components/Fooditem2.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="mango">
+    <div  class="mango">
       <h2>{{ name }}</h2>
-      <p>{{ message } }</p>
+      <p>{{ message }}</p>
     </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      name: 'Mangoes',
-      message: 'I like Mangoes'
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Mangoes',
+        message: 'I like Mangoes'
+      }
     }
+  };
+  </script>
+  
+  <style>
+    .mango {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightyellow;
   }
-};
-</script>
-
-<style>
-  .mango {
-  border: dashed black 1px;
-  display: inline-block;
-  margin: 10px;
-  padding: 10px;
-  background-color:
-}
-</style>
+  </style>
