a79332e2ea833a760bba375ec228711d3b5fd81a
apcpdarquines <pdarquines@student.apc.edu.ph>
2025-03-01 08:52:39 +0000

    mango and apple

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h2>{{ name }}</h2>
-    <p>{{ message }}</p>
-  </div>
+    <div class="apple">
+      <h2>{{ name }}</h2>
+      <p>{{ message } }</p>
+    </div>
 </template>
 
 <script>
@@ -17,11 +17,11 @@ export default {
 </script>
 
 <style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
+  .apple {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color:
+}
 </style>
